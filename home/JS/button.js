90922afb2958da72025c2564361d5ac31965a33f
samgongora26 <saul.santiago2426@gmail.com>
2019-11-23 00:56:12 -0600

    promociones agregadas al home

--- home/JS/button.js
+++ home/JS/button.js
@@ -2,8 +2,42 @@ $(document).ready(function(){
   //alert("hola");
 
 });
-    
+
 function abajo(id){
+  
+  var categ = $(this).attr('category');
+  console.log(categ);
+
+  if (document.getElementById(id).style.height == "365px") {
+      //$(' .escondido[category="item1"]').addClass('activarSty');
+      document.getElementById(id).style.height = "400px";
+      document.getElementById(id).style.grid.row.end = "span 4";
+      //$('.escondido[category="item1"]').addClass('activarSty');
+      
+    } else {
+      //$(' .escondido[category="item1"]').removeClass('activarSty');
+      document.getElementById(id).style.height = "365px";
+    }
+
+    acordeon(item,id);
+
+} 
+
+function acordeon(item,id){
+  //funcion que llama al query que agrega o quita la clase css para el boton ver más
+  //var categ = $(this).attr('category');
+  //console.log(categ);
+
+  if (document.getElementById(id).style.height == "365px") {
+      $(' .escondido[category="item1"]').addClass('activarSty');
+
+    } else {
+      $(' .escondido[category="item1"]').removeClass('activarSty');
+
+    }
+}
+
+/*function abajo(id){
   
   if (document.getElementById(id).style.height == "365px") {
       $('.post1 .escondido[category="item1"]').addClass('activarSty');
@@ -15,13 +49,9 @@ function abajo(id){
       $('.post1 .escondido[category="item1"]').removeClass('activarSty');
       document.getElementById(id).style.height = "365px";
     }
-} 
+} */
 
-function acordeon(item){
-  //funcion que llama al query que agrega o quita la clase css para el boton ver más
-  $('.post1 .escondido[category="item1"]').addClass('activarSty');
 
-}
 
 
 
