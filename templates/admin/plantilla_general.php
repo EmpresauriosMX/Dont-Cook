013c0ec132318220e0254aa8cf557c77cabc6b4d
satswere <verjruhfukeufn@gmail.com>
2021-05-05 16:31:00 -0500

    creacion plantilla general con sus componentes

---
+++ templates/admin/plantilla_general.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <?php 
+    include '../../templates/componentes/header.html';
+    ?>
+</head>
+<body>
+   <?php
+   include '../../templates/componentes/head.html';
+   include '../../templates/componentes/navegacion.html';
+   ?>
+   <!--contenido de la plantilla -->
+
+
+<h2>el contenido va aqui</h2>
+
+
+<!--contenido de la plantilla -->
+    <?php 
+    include '../../templates/componentes/footer.html';
+    include '../../templates/componentes/scripts.html';
+    ?>
+</body>
+
+</html>
