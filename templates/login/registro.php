3b70c02461c89061f75c92eba672b0e9bb2b87c6
silvercrow185 <oossllyy14@gmail.com>
2021-05-08 15:25:45 -0500

    fondo del login y registro

--- templates/login/registro.php
+++ templates/login/registro.php
@@ -10,7 +10,7 @@
     <body>
         
         <div class="container">
-            <div style="background:#00000099; color:white; font-weight:bold; padding:15px; border:3px solid #B34F19; margin-top:40px; margin-bottom:40px; text-align:center; font-size:22px; border-radius:10px;">
+            <div style="background:#00000099; color:white; font-weight:bold; padding:15px; border:3px solid #B34F19; margin-top:40px; margin-bottom:40px; text-align:center; font-size:13px; border-radius:10px;">
                 <h1>Registro</h1>
                 <form>
                     <input type="text" name="Nombre" placeholder="Nombre">
