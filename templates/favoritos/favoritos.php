f5f3f61daed9fe3bdbb53e98b3f1295409acd87e
samgongora26 <saul.santiago2426@gmail.com>
2021-05-09 21:20:28 -0500

    favoritos

---
+++ templates/favoritos/favoritos.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php 
+    include '../componentes/header.html';
+    ?>
+</head>
+<body>
+   <?php
+   include '../componentes/head.html';
+   include '../componentes/navegacion_reducido.html';
+   ?>
+   <!--contenido de la plantilla -->
+
+    <div class="container">
+        
+
+        <!--RESTAURANTES-->
+        <div class="section-title">
+            <h2>FAVORITOS <span class="fa fa-heart"></span></h2>
+        </div>
+        <div class="row">
+            <!--restaurante 1-->
+            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
+                <div class="featured__item">
+                    <div class="featured__item__pic set-bg" data-setbg="../../src/img/restaurants/pikalogodarkmode.png">
+                        <ul class="featured__item__pic__hover">
+                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                            <li><a href="#"><i class="fa fa-eye"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="featured__item__text">
+                        <h6><a href="#">pequeña descripción</a></h6>
+                        <h5>Restaurante</h5>
+                    </div>
+                </div>
+            </div>
+            <!--restaurante 2-->
+            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
+                <div class="featured__item">
+                    <div class="featured__item__pic set-bg" data-setbg="../../src/img/restaurants/pikalogodarkmode.png">
+                        <ul class="featured__item__pic__hover">
+                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                            <li><a href="#"><i class="fa fa-eye"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="featured__item__text">
+                        <h6><a href="#">pequeña descripción</a></h6>
+                        <h5>Restaurante</h5>
+                    </div>
+                </div>
+            </div>
+            <!--restaurante 3-->
+            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
+                <div class="featured__item">
+                    <div class="featured__item__pic set-bg" data-setbg="../../src/img/restaurants/pikalogodarkmode.png">
+                        <ul class="featured__item__pic__hover">
+                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                            <li><a href="#"><i class="fa fa-eye"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="featured__item__text">
+                        <h6><a href="#">pequeña descripción</a></h6>
+                        <h5>Restaurante</h5>
+                    </div>
+                </div>
+            </div>
+            <!--restaurante 4-->
+            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
+                <div class="featured__item">
+                    <div class="featured__item__pic set-bg" data-setbg="../../src/img/restaurants/pikalogodarkmode.png">
+                        <ul class="featured__item__pic__hover">
+                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                            <li><a href="#"><i class="fa fa-eye"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="featured__item__text">
+                        <h6><a href="#">pequeña descripción</a></h6>
+                        <h5>Restaurante</h5>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+
+
+    </div>
+
+
+
+<!--contenido de la plantilla -->
+    <?php 
+    include '../componentes/footer.html';
+    include '../componentes/scripts.html';
+    ?>
+</body>
+
+</html>
