b471ddee51a33057de02159de65221a96a2acc19
samgongora26 <saul.santiago2426@gmail.com>
2021-05-10 22:40:44 -0500

    componente de categorias e imagenes reducidas

--- templates/home/home.php
+++ templates/home/home.php
@@ -13,45 +13,14 @@
    ?>
 
 <!--contenido de la plantilla -->
-    <div class="container">
-        <h3>Categorías destacadas</h3>
-    </div>
+    
     <!--Todo de home-->
      
-     <section class="categories mt-3">
+     <section class="categories ">
         <div class="container">
-            <!-- Slider de categorias -->
-            <div class="row mb-3">
-                <div class="categories__slider owl-carousel">
-                    <div class="col-lg-3">
-                        <div class="categories__item set-bg" data-setbg="../../src/img/categories/fast_food.png">
-                            <h5><a href="#">Fast food</a></h5>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="categories__item set-bg" data-setbg="../../src/img/categories/tacos.png">
-                            <h5><a href="#">Taquerías</a></h5>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="categories__item set-bg" data-setbg="../../src/img/categories/domicilio.png">
-                            <h5><a href="#">Con entrega a domicilio</a></h5>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="categories__item set-bg" data-setbg="../../src/img/categories/pizza.png">
-                            <h5><a href="#">Pizzerías</a></h5>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="categories__item set-bg" data-setbg="../../src/img/categories/carritos.png">
-                            <h5><a href="#">Carritos de comida</a></h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br>
-            <!-- Fin slider de categorias -->
+        <?php
+            include '../../templates/componentes/categorias.html';
+        ?>
 
             <!--RESTAURANTES-->
             <div class="section-title mt-3">
