7fb5c48aaf4c927474840eb6e68cabb17f2197e2
satswere <verjruhfukeufn@gmail.com>
2021-05-25 14:26:22 -0500

    registro de nuevos usuarios
    se ha creado la funcion de crear nuevos usarios a la base de datos con claves cifradas, estructura base para poder funcionar, se agregaran despues funciones como validaciones y optimizaciones de los input

--- inc/funciones/login/registro.js
+++ inc/funciones/login/registro.js
@@ -1,6 +1,7 @@
-const registro = document.querySelector("#form_registro");
-const url = "inc/peticiones/login/funciones.php";
+import { enviar_datos } from "../../funciones_generales.js";
 
+const registro = document.querySelector("#form_registro");
+const url = "../../inc/peticiones/login/funciones.php";
 document.addEventListener("DOMContentLoaded",() =>{
     registro.addEventListener("submit",registro_usuario);
 });
@@ -12,8 +13,19 @@ function registro_usuario(e){
     const correo = document.querySelector("#correo").value;
     const usuario = document.querySelector("#usuario").value;
     const contrasenia1 = document.querySelector("#contrasenia1").value;
-    const contrasenia2 = document.querySelector("#contrasenia2").value;
+//    const contrasenia2 = document.querySelector("#contrasenia2").value;
+console.log(`${nombres}, ${apellidos}, ${correo}, ${usuario}, ${contrasenia1}`)
+ const datos = new FormData();
+ datos.append('nombres',nombres);
+ datos.append('apellidos',apellidos);
+ datos.append('correo',correo);
+ datos.append('usuario',usuario);
+ datos.append('contrasenia1',contrasenia1);
+ datos.append('accion','registrar');
 
+enviar_datos(url,datos).then((re) => console.log(re));
+
+/*
     if(contrasenia1 == contrasenia2){
         const datos = new FormData();
         //datos.append("accion","registrar_usuario");
@@ -22,15 +34,14 @@ function registro_usuario(e){
             if(res.respuesta == 1){
                 //si la respuesta es ok
                 console.log("respuesta = ok");
-            }
+            }*/
             /*res.forEach((municipio) => {
               cbx_municipio.innerHTML += `
                     <option value="${municipio.id}">${municipio.nombre}</option>  
                     `;
             });*/   
-          });
-    }
-    alert(nombres);
+       //   });
+    //}
     //console.log(acc);
 }
 
--- inc/peticiones/login/consultas.php
+++ inc/peticiones/login/consultas.php
@@ -1,13 +1,42 @@
 <?php
-function getEstado(): array
+
+function nuevo_usuario(): array
 {
+    $opciones =array('cost' => 12);
+
+    $nombre = $_POST['nombres'];
+    $apellido = $_POST['apellidos'];
+    $correo = $_POST['correo'];
+    $usuario =$_POST['usuario'];
+    $contra =$_POST['contrasenia1'];
+    $type_user = 2;
+
+    $hash_password  =password_hash($contra,PASSWORD_BCRYPT,$opciones);
+
     try {
         require '../../../conexion.php';
-        $hola = "hola";
-        //var_dump($estados);
-        return $hola;
+        $stmt = $conn->prepare("INSERT INTO usuarios (nombres, apellidos, correo, usuario, contraseña, tipo_usuario) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param('sssssi', $nombre, $apellido, $correo, $usuario,$hash_password,$type_user);
+        $stmt -> execute();
+
+        $respuesta = array(
+            'respuesta' =>  $stmt ->affected_rows,
+            'nombre' => $nombre,
+            'apellido'=> $apellido,
+            'correo' => $correo,
+            'tipo' => $type_user,
+            'user' => $usuario,
+            'pass' => $contra,
+            'pass_hast' => $hash_password
+        );
+
+       $stmt->close();
+        return $respuesta;
+
     } catch (\Throwable $th) {
-        var_dump($th);
+        $respuesta = array(
+            'respuesta' => $th
+        );
+        return $respuesta;
     }
-    mysqli_close($conn);
 }
