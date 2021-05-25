51d517f5e55030a3010048252ca9bd103ad49416
silvercrow185 <oossllyy14@gmail.com>
2021-05-25 14:59:07 -0500

    promociones v.1

---
+++ inc/peticiones/promociones/todas_promociones.php
@@ -0,0 +1,82 @@
+<div class="card-columns">
+    <!--Tarjeta de Base-->
+    <div class="card">
+        <img class="card-img-top" src="../../src/img/banner/banner-1.jpg" alt="Card image cap">
+        <div class="card-img-overlay">
+        <h4 class="card-title">Restaurante</h4>
+    </div>
+    <div class="card-body">
+        <h6>Nombre de la Promoción</h6>
+        <p class="card-text">Descripción de la promocion... <br>
+        Chelas 2x1 xdxd <br>
+        De Lunes a Jueves <br>
+        Con Horario de 12:00 a 16:00</p>
+    </div>
+</div>
+<!--Fin tarjeta de Base-->
+
+<div class="card">
+    <img class="card-img-top" src="../../src/img/banner/banner-2.jpg" alt="Card image cap">
+    <div class="card-img-overlay">
+    <h4 class="card-title">Restaurante</h4>
+</div>
+<div class="card-body">
+<h6>Nombre de la Promoción</h6>
+<p class="card-text">Descripción de la promocion... <br>
+Chelas 2x1 xdxd <br>
+De Lunes a Jueves <br>
+Con Horario de 12:00 a 16:00</p>
+</div>
+</div>
+
+<div class="card">
+<img class="card-img-top" src="../../src/img/banner/banner_dc.png" alt="Card image cap">
+<div class="card-img-overlay text-white">
+<h4 class="card-title">Restaurante</h4>
+</div>
+<div class="card-body">
+<h6>Nombre de la Promoción</h6>
+<p class="card-text">Descripción de la promocion... <br>
+Chelas 2x1 xdxd <br>
+De Lunes a Jueves <br>
+Con Horario de 12:00 a 16:00</p>
+</div>
+</div>
+
+<div class="card">
+<img class="card-img-top" src="../../src/img/banner/banner_dc2.png" alt="Card image cap">
+<div class="card-img-overlay text-white">
+<h4 class="card-title">Restaurante</h4>
+</div>
+<div class="card-body">
+<h6>Nombre de la Promoción</h6>
+<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+</div>
+</div>
+
+<div class="card">
+<img class="card-img-top" src="../../src/img/banner/banner_dc6.png" alt="Card image cap">
+<div class="card-img-overlay text-white">
+<h4 class="card-title">Restaurante</h4>
+</div>
+<div class="card-body">
+<h6>Nombre de la Promoción</h6>
+<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo reprehenderit necessitatibus cumque non cupiditate, odio commodi eveniet modi tenetur! Quis rem perspiciatis enim nesciunt deleniti doloremque libero aut quo natus.</p>
+</div>
+</div>
+
+<div class="card">
+<img class="card-img-top" src="../../src/img/banner/banner_dc4.png" alt="Card image cap">
+<div class="card-img-overlay text-white">
+<h4 class="card-title">Restaurante</h4>
+</div>
+<div class="card-body">
+<h6>Nombre de la Promoción</h6>
+<p class="card-text">Descripción de la promocion... <br>
+Chelas 2x1 xdxd <br>
+De Lunes a Jueves <br>
+Con Horario de 12:00 a 16:00</p>
+</div>
+</div>
+</div>
+<!--Fin de tarjetas de promociones-->
--- templates/promociones/promociones.php
+++ templates/promociones/promociones.php
@@ -17,7 +17,7 @@
     </div>
     <!--Todo de home-->
      
-     <section class="categories">
+    <section class="categories">
         <div class="container">
             <br>
             <!-- Fin slider de categorias -->
@@ -27,193 +27,40 @@
                 <h2>Promociones de Hoy!</h2>
             </div>
             <div class="card-columns">
-                <!--Tarjeta de Base-->
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner-1.jpg" alt="Card image cap">
-                    <div class="card-img-overlay">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Descripción de la promocion... <br>
-                        Chelas 2x1 xdxd <br>
-                        De Lunes a Jueves <br>
-                        Con Horario de 12:00 a 16:00</p>
-                    </div>
-                    <!--<div class="card-footer bg-danger">
-                        <ul class="text-center">
-                            <a href="#"><img width="30" height="30"  src="../../src/img/iconos/ubicacion.png"></a>
-                            <a href="#"><img width="30" height="30" src="../../src/img/iconos/tripadvisor.png"></a>
-                            <a href="#"><img width="30" height="30" src="../../src/img/iconos/facebook.png"></a>
-                            <a href="#"><img width="30" height="30" src="../../src/img/iconos/instagram.png"></a>
-                            <a href="#"><img width="30" height="30" src="../../src/img/iconos/galeria.png"></a>
-                        </ul>
-                    </div>-->
-                </div>
-                <!--Fin tarjeta de Base-->
-
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner-2.jpg" alt="Card image cap">
-                    <div class="card-img-overlay">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Descripción de la promocion... <br>
-                        Chelas 2x1 xdxd <br>
-                        De Lunes a Jueves <br>
-                        Con Horario de 12:00 a 16:00</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner_dc.png" alt="Card image cap">
-                    <div class="card-img-overlay text-white">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Descripción de la promocion... <br>
-                        Chelas 2x1 xdxd <br>
-                        De Lunes a Jueves <br>
-                        Con Horario de 12:00 a 16:00</p>
-                    </div>
-                </div>
-                
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner_dc2.png" alt="Card image cap">
-                    <div class="card-img-overlay text-white">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner_dc6.png" alt="Card image cap">
-                    <div class="card-img-overlay text-white">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo reprehenderit necessitatibus cumque non cupiditate, odio commodi eveniet modi tenetur! Quis rem perspiciatis enim nesciunt deleniti doloremque libero aut quo natus.</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner_dc4.png" alt="Card image cap">
-                    <div class="card-img-overlay text-white">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Descripción de la promocion... <br>
-                        Chelas 2x1 xdxd <br>
-                        De Lunes a Jueves <br>
-                        Con Horario de 12:00 a 16:00</p>
-                    </div>
-                </div>
+                <?php
+                    $day = date("l");
+                    switch ($day) {
+                        case "Sunday":
+                            include '../../inc/peticiones/promociones/promociones_domingo.php';
+                        break;
+                        case "Monday":
+                            include '../../inc/peticiones/promociones/promociones_lunes.php';
+                        break;
+                        case "Tuesday":
+                            include '../../inc/peticiones/promociones/promociones_martes.php';
+                        break;
+                        case "Wednesday":
+                            include '../../inc/peticiones/promociones/promociones_miercoles.php';
+                        break;
+                        case "Thursday":
+                            include '../../inc/peticiones/promociones/promociones_jueves.php';
+                        break;
+                        case "Friday":
+                            include '../../inc/peticiones/promociones/promociones_viernes.php';
+                        break;
+                        case "Saturday":
+                            include '../../inc/peticiones/promociones/promociones_sabado.php';
+                        break;
+                    }
+                ?>
+                <!--Fin de tarjetas de promociones-->
             </div>
-            <!--Fin de tarjetas de promociones-->
-
             <!--TODAS LAS PROMOCIONES-->
             <!--Tarjetas de promociones-->
             <div class="section-title mt-3">
                 <h2>Todas las promociones</h2>
             </div>
-            <div class="card-columns">
-                <!--Tarjeta de Base-->
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner-1.jpg" alt="Card image cap">
-                    <div class="card-img-overlay">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Descripción de la promocion... <br>
-                        Chelas 2x1 xdxd <br>
-                        De Lunes a Jueves <br>
-                        Con Horario de 12:00 a 16:00</p>
-                    </div>
-                    <!--<div class="card-footer bg-danger">
-                        <ul class="text-center">
-                            <a href="#"><img width="30" height="30"  src="../../src/img/iconos/ubicacion.png"></a>
-                            <a href="#"><img width="30" height="30" src="../../src/img/iconos/tripadvisor.png"></a>
-                            <a href="#"><img width="30" height="30" src="../../src/img/iconos/facebook.png"></a>
-                            <a href="#"><img width="30" height="30" src="../../src/img/iconos/instagram.png"></a>
-                            <a href="#"><img width="30" height="30" src="../../src/img/iconos/galeria.png"></a>
-                        </ul>
-                    </div>-->
-                </div>
-                <!--Fin tarjeta de Base-->
-
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner-2.jpg" alt="Card image cap">
-                    <div class="card-img-overlay">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Descripción de la promocion... <br>
-                        Chelas 2x1 xdxd <br>
-                        De Lunes a Jueves <br>
-                        Con Horario de 12:00 a 16:00</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner_dc.png" alt="Card image cap">
-                    <div class="card-img-overlay text-white">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Descripción de la promocion... <br>
-                        Chelas 2x1 xdxd <br>
-                        De Lunes a Jueves <br>
-                        Con Horario de 12:00 a 16:00</p>
-                    </div>
-                </div>
-                
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner_dc2.png" alt="Card image cap">
-                    <div class="card-img-overlay text-white">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner_dc6.png" alt="Card image cap">
-                    <div class="card-img-overlay text-white">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo reprehenderit necessitatibus cumque non cupiditate, odio commodi eveniet modi tenetur! Quis rem perspiciatis enim nesciunt deleniti doloremque libero aut quo natus.</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img class="card-img-top" src="../../src/img/banner/banner_dc4.png" alt="Card image cap">
-                    <div class="card-img-overlay text-white">
-                        <h4 class="card-title">Restaurante</h4>
-                    </div>
-                    <div class="card-body">
-                    <h6>Nombre de la Promoción</h6>
-                    <p class="card-text">Descripción de la promocion... <br>
-                        Chelas 2x1 xdxd <br>
-                        De Lunes a Jueves <br>
-                        Con Horario de 12:00 a 16:00</p>
-                    </div>
-                </div>
-            </div>
-            <!--Fin de tarjetas de promociones-->
+            <?php include '../../inc/peticiones/promociones/todas_promociones.php'; ?>
         </div>
     </section>
    <!--Fin de todo de home-->
