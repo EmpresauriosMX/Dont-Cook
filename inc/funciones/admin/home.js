e49ae7d03b18ac8310e7daf1e5bd4ce42c3cad2f
samgongora26 <saul.santiago2426@gmail.com>
2021-05-27 23:17:56 -0500

    sustitudo del switch en js

--- inc/funciones/admin/home.js
+++ inc/funciones/admin/home.js
@@ -25,6 +25,13 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("existe una cuenta");
             cargar_restaurantes();
         }
+        else{
+            //NO EXISTE UNA CUENTA
+            //mostrar el mensaje de no existe una cuenta 
+            let mensaje = "sin_cuenta";
+            mostrar_mensaje(mensaje);
+            console.log("no existe una cuenta");
+        }
 
     }
 
@@ -37,5 +44,28 @@ document.addEventListener("DOMContentLoaded", () => {
         //enviar_datos(url,datos).then((re) => console.log(re));
 
         //alert('EL RESTAURANTE SE REGISTRO CORRECTAMENTE');
+
+    }
+
+    function cargar_restaurantes(){
+        //e.preventDefault();
+        const datos = new FormData();
+        console.log("ahora puedo buscar restaurantes")
+        //datos.append("accion","registrar_restaurante");
+    
+        //enviar_datos(url,datos).then((re) => console.log(re));
+
+        //alert('EL RESTAURANTE SE REGISTRO CORRECTAMENTE');
+
+    }
+
+    function mostrar_mensaje(mensaje){
+        const MENSAJES_POR_MOSTRAR ={
+            'sin_cuenta' :  "mostrar mensaje de que no hay cuenta",
+            //'existe_cuenta' : "mostrar mensaje de que  si hay cuenta",
+            'sin_restaurante'  : "mostrar mensaje de que si hay cuenta"
+        }
+        const muestra = MENSAJES_POR_MOSTRAR[mensaje];
+        console.log(muestra);
 
     }
