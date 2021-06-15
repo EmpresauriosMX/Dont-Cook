4546e9cbe7a70996de109eb88f402b4039b3339b
feltydany <feltydany.dgm@gmail.com>
2021-06-14 20:47:32 -0500

    Tarjetas en componentes y reacomodadas
    Signed-off-by: feltydany <feltydany.dgm@gmail.com>

--- templates/root/root_restaurantes_pendientes.php
+++ templates/root/root_restaurantes_pendientes.php
@@ -25,69 +25,11 @@
         <div class="section-title mt-3">
             <h2>Restaurantes por aprobar</h2>
         </div>
-        <div class="row mt-3">
-            <!--restaurante 1-->
-            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
-                <div class="featured__item">
-                    <div class="featured__item__pic set-bg" data-setbg="../../src/img/restaurants/pikalogodarkmode.png">
-                        <ul class="featured__item__pic__hover">
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-eye"></i></a></li>
-                        </ul>
-                    </div>
-                    <div class="featured__item__text">
-                        <h6><a href="#">pequeña descripción</a></h6>
-                        <h5>Restaurante</h5>
-                    </div>
-                </div>
-            </div>
-            <!--restaurante 2-->
-            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
-                <div class="featured__item">
-                    <div class="featured__item__pic set-bg" data-setbg="../../src/img/restaurants/pikalogodarkmode.png">
-                        <ul class="featured__item__pic__hover">
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-eye"></i></a></li>
-                        </ul>
-                    </div>
-                    <div class="featured__item__text">
-                        <h6><a href="#">pequeña descripción</a></h6>
-                        <h5>Restaurante</h5>
-                    </div>
-                </div>
-            </div>
-            <!--restaurante 3-->
-            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
-                <div class="featured__item">
-                    <div class="featured__item__pic set-bg" data-setbg="../../src/img/restaurants/pikalogodarkmode.png">
-                        <ul class="featured__item__pic__hover">
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-eye"></i></a></li>
-                        </ul>
-                    </div>
-                    <div class="featured__item__text">
-                        <h6><a href="#">pequeña descripción</a></h6>
-                        <h5>Restaurante</h5>
-                    </div>
-                </div>
-            </div>
-            <!--restaurante 4-->
-            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
-                <div class="featured__item">
-                    <div class="featured__item__pic set-bg" data-setbg="../../src/img/restaurants/pikalogodarkmode.png">
-                        <ul class="featured__item__pic__hover">
-                            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                            <li><a href="#"><i class="fa fa-eye"></i></a></li>
-                        </ul>
-                    </div>
-                    <div class="featured__item__text">
-                        <h6><a href="#">pequeña descripción</a></h6>
-                        <h5>Restaurante</h5>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+        
+        <!--TARJETA DE RESTAURANTES-->
+        <?php
+            include '../componentes/tarjeta_restaurante.html';
+        ?>
 
         
     </div>
