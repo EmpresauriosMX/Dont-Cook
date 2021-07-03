9f67edb5733b1758ff67e8abf42cebde2beea9ba
feltydany <feltydany.dgm@gmail.com>
2021-07-03 18:15:50 -0500

    Aviso de privacidad corregido
    Signed-off-by: feltydany <feltydany.dgm@gmail.com>

--- templates/pages/aviso_privacidad.php
+++ templates/pages/aviso_privacidad.php
@@ -9,32 +9,82 @@
 <body>
    <?php
    include '../../templates/componentes/head.html';
-   include '../../templates/componentes/navegacion.html';
+   include '../componentes/navegacion_reducido.html';
    ?>
 
 <!--contenido de la plantilla -->
 
-    <section>
-
-        <div class="card text-center">
-            <div class="card-header">
-               <h1>Aviso de privacidad simplificado</h1>
-            </div>
-            <div class="card-body">
-                <p class="card-text">Daniel Gonzalez Mejia mejor conocido como Don't Cook, con domicilio en Cerrada Faro, Puerto Maya, Solidaridad, Quintana Roo., es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</p>
-                <h5 class="card-title">¿Para qué fines utilizaremos sus datos personales?</h5>
-                <p class="card-text">Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</p>
-                <h5 class="card-tittle">¿Dónde puedo consultar el aviso de privacidad integral?</h5>
-                <p class=card-text>Para conocer mayor información sobre los términos y condiciones en que serán tratados sus datos personales, como los terceros con quienes compartimos su información personal y la forma en que podrá ejercer sus derechos ARCO, puede consultar el aviso de privacidad integral con una petición vía correo electrónico:</p>
-                <p class=cart-text>dont.cook.mx@gmail.com</p>
-
-            </div>
-            <div class="card-footer text-muted">
-                <p class="card-text">Última actualización de este <a href="https://markethax.com/generador-aviso-de-privacidad/">aviso de privacidad</a> : 03/07/2021</p>
-            </div>
+<container>
+    <div class="card text-center">
+        <div class="card-header">
+            <h1>Aviso de privacidad simplificado</h1>
         </div>
+        <div class="card-body">
+            <p class="card-text">Daniel Gonzalez Mejia mejor conocido como Don't Cook, con domicilio en Cerrada Faro, es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</p>
+            <h5 class="card-title">¿Para qué fines utilizaremos sus datos personales?</h5>
+            <p class="card-text">Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</p>
+            <h5 class="card-tittle">¿Dónde puedo consultar el aviso de privacidad integral?</h5>
+            <p class=card-text>Para conocer mayor información sobre los términos y condiciones en que serán tratados sus datos personales, como los terceros con quienes compartimos su información personal y la forma en que podrá ejercer sus derechos ARCO, puede consultar el aviso de privacidad integral con una petición vía correo electrónico:</p>
+            <p class=cart-text>dont.cook.mx@gmail.com</p>
 
-    </section>
+        </div>
+        <div class="card-footer text-muted">
+            <p class="card-text text-center">Última actualización de este aviso de privacidad: 03/07/2021</p>
+        </div>
+    </div>
+<br>
+    <div class="card">
+        <div class="card-header text-center">
+            <h1>Aviso de privacidad integral</h1>
+        </div>
+        <div class="card-body">
+            <p class="card-text">Daniel Gonzalez Mejia mejor conocido como Don\'t Cook, con domicilio en Cerrada Faro #112, Puerto Maya, Solidaridad, Quintana Roo, México y portal de internet dontcook.mx, es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</p>
+            
+            <h3 class="card-title text-center">¿Para qué fines utilizaremos sus datos personales?</h3>
+            <p class="card-text">Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</p>
+            <p class="card-text">Respuesta a mensajes del formulario de contacto</p>
+            <p class="card-text">Comerciales, Publicidad</p>
+
+            <h3 class="card-tittle text-center">¿Qué datos personales utilizaremos para estos fines?</h3>
+            <p class="card-text">Para llevar a cabo las finalidades descritas en el presente aviso de privacidad, utilizaremos los siguientes datos personales:</p>
+            <p class="cart-text">Datos de identificación y contacto, Datos laborales</p>
+
+            <h3 class="card-tittle text-center">¿Con quién compartimos su información personal y para qué fines?</h3>
+            <p class="card-text">Le informamos que sus datos personales son compartidos fuera del país con las siguientes personas, empresas, organizaciones o autoridades distintas a nosotros, para los siguientes fines:</p>
+            <p class="card-text">Dentro del país</p>
+            <p class="card-text">Usuarios no registrados en la página</p>
+
+            <h3 class="card-tittle text-center">¿Cómo puede acceder, rectificar o cancelar sus datos personales, u oponerse a su uso o ejercer la revocación de consentimiento?</h3>
+            <p class="card-text">Usted tiene derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea inexacta o incompleta (Rectificación); que la eliminemos de nuestros registros o bases de datos cuando considere que la misma no está siendo utilizada adecuadamente (Cancelación); así como oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como derechos ARCO.</p>
+            <p class="card-text">Para el ejercicio de cualquiera de los derechos ARCO, debe enviar una petición vía correo electrónico a dont.cook.mx@gmail.com y deberá contener:</p>
+            <p class="card-text">· Nombre completo del titular.</p>
+            <p class="card-text">· Domicilio.</p>
+            <p class="card-text">· Teléfono.</p>
+            <p class="card-text">· Correo electrónico usado en este sitio web.</p>
+            <p class="card-text">· Copia de una identificación oficial adjunta.</p>
+            <p class="card-text">· Asunto «Derechos ARCO»</p>
+
+            <p class="card-text">Descripción el objeto del escrito, los cuales pueden ser de manera enunciativa más no limitativa los siguientes: Revocación del consentimiento para tratar sus datos personales; y/o Notificación del uso indebido del tratamiento de sus datos personales; y/o Ejercitar sus Derechos ARCO, con una descripción clara y precisa de los datos a Acceder, Rectificar, Cancelar o bien, Oponerse. En caso de Rectificación de datos personales, deberá indicar la modificación exacta y anexar la documentación soporte; es importante en caso de revocación del consentimiento, que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligación legal requiramos seguir tratando sus datos personales. Asimismo, usted deberá considerar que para ciertos fines, la revocación de su consentimiento implicará que no le podamos seguir prestando el servicio que nos solicitó, o la conclusión de su relación con nosotros.</p>
+            <p class="card-text text-center">¿En cuántos días le daremos respuesta a su solicitud?</p>
+            <p class="card-text">5 Días</p>
+            <p class="card-text text-center">¿Por qué medio le comunicaremos la respuesta a su solicitud?</p>
+            <p class="card-text">Al mismo correo electrónico de donde se envío la petición.</p>
+
+            <h3 class="card-tittle text-center">El uso de tecnologías de rastreo en nuestro portal de internet</h3>
+            <p class="card-text">Le informamos que en nuestra página de internet utilizamos cookies, web beacons u otras tecnologías, a través de las cuales es posible monitorear su comportamiento como usuario de internet, así como brindarle un mejor servicio y experiencia al navegar en nuestra página. Los datos personales que obtenemos de estas tecnologías de rastreo son los siguientes:</p>
+            <p class="card-text">Identificadores, nombre de usuario y contraseñas de sesión, Región en la que se encuentra el usuario, Fecha y hora del inicio y final de una sesión de un usuario</p>
+            <p class="card-text">Estas cookies, web beacons y otras tecnologías pueden ser deshabilitadas. Para conocer cómo hacerlo, consulte el menú de ayuda de su navegador. Tenga en cuenta que, en caso de desactivar las cookies, es posible que no pueda acceder a ciertas funciones personalizadas en nuestros sitio web.</p>
+
+            <h3 class="card-tittle text-center">¿Cómo puede conocer los cambios en este aviso de privacidad?</h3>
+            <p class="card-text">El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras propias necesidades por los productos o servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios en nuestro modelo de negocio, o por otras causas. Nos comprometemos a mantener actualizado este aviso de privacidad sobre los cambios que pueda sufrir y siempre podrá consultar las actualizaciones que existan en el sitio web <a href="https://dontcook.mx">dontcook.mx</a>.</p>
+            
+
+        </div>
+        <div class="card-footer text-muted">
+            <p class="card-text text-center">Última actualización de este aviso de privacidad: 03/07/2021</p>
+        </div>
+    </div>
+</container>
 
 
 <!--contenido de la plantilla -->
