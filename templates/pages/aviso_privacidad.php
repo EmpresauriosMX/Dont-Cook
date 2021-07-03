cead4a17a8d37523e105d439ce35fcf95ab5da23
feltydany <feltydany.dgm@gmail.com>
2021-07-03 17:48:30 -0500

    Aviso de privacidad
    Signed-off-by: feltydany <feltydany.dgm@gmail.com>

---
+++ templates/pages/aviso_privacidad.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <?php 
+    include '../../templates/componentes/header.html';
+    ?>
+</head>
+<body>
+   <?php
+   include '../../templates/componentes/head.html';
+   include '../../templates/componentes/navegacion.html';
+   ?>
+
+<!--contenido de la plantilla -->
+
+    <section>
+
+        <div class="card text-center">
+            <div class="card-header">
+               <h1>Aviso de privacidad simplificado</h1>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Daniel Gonzalez Mejia mejor conocido como Don't Cook, con domicilio en Cerrada Faro, Puerto Maya, Solidaridad, Quintana Roo., es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</p>
+                <h5 class="card-title">¿Para qué fines utilizaremos sus datos personales?</h5>
+                <p class="card-text">Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</p>
+                <h5 class="card-tittle">¿Dónde puedo consultar el aviso de privacidad integral?</h5>
+                <p class=card-text>Para conocer mayor información sobre los términos y condiciones en que serán tratados sus datos personales, como los terceros con quienes compartimos su información personal y la forma en que podrá ejercer sus derechos ARCO, puede consultar el aviso de privacidad integral con una petición vía correo electrónico:</p>
+                <p class=cart-text>dont.cook.mx@gmail.com</p>
+
+            </div>
+            <div class="card-footer text-muted">
+                <p class="card-text">Última actualización de este <a href="https://markethax.com/generador-aviso-de-privacidad/">aviso de privacidad</a> : 03/07/2021</p>
+            </div>
+        </div>
+
+    </section>
+
+
+<!--contenido de la plantilla -->
+    <script src="../../inc/funciones/home/home.js" type="module"></script>
+    <script src="../../inc/funciones/home/categorias.js" type="module"></script>
+    <!--script src="../../inc/funciones/restaurantes/app.js" type="module"></script-->
+
+    <?php 
+    include '../../templates/componentes/footer.html';
+    include '../../templates/componentes/scripts.html';
+    ?>
+</body>
+
+</html>
