afb985a922c072d4a41540f9e9db73ba0042125d
feltydany <feltydany.dgm@gmail.com>
2021-07-09 23:56:04 -0500

    Limpio codigo
    Signed-off-by: feltydany <feltydany.dgm@gmail.com>

--- templates/admin/agregar_restaurante.php
+++ templates/admin/agregar_restaurante.php
@@ -31,7 +31,7 @@ include '../../inc/peticiones/login/sesion.php';
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="breadcrumb__text">
-                        <h2 id="nombre_restaurante">Agregar Restaurante</h2>
+                        <h2 id="nombre_restaurante">Agregar Nuevo Restaurante</h2>
                     </div>
                 </div>
             </div>
@@ -49,7 +49,7 @@ include '../../inc/peticiones/login/sesion.php';
                 <div class="col-lg-8 col-md-6 mx-auto">
                     
                     <div class="checkout__input text-center">
-                        <h3 class="h3">Información del Restaurante<span>*</span></h3>
+                        <h3 class="h3">Información del Restaurante<span></span></h3>
                     </div>
                     <div class="row">
 
@@ -86,7 +86,7 @@ include '../../inc/peticiones/login/sesion.php';
                     </div>
 
                     <div class="checkout__input">
-                        <h6 class="h6">Selecciona la ciudad en la que se encuentra<span>*</span></h6>
+                        <h6 class="h6">Selecciona la ciudad en la que se encuentra<span></span></h6>
                     </div>
 
                     <div class="input-group mb-3">
@@ -118,7 +118,7 @@ include '../../inc/peticiones/login/sesion.php';
 
 
                     <div class="checkout__input text-center">
-                        <h3 class="h3">Información de Contacto<span>*</span></h3>
+                        <h3 class="h3">Información de Contacto<span></span></h3>
                     </div>
                     <div class="row">
 
@@ -159,7 +159,7 @@ include '../../inc/peticiones/login/sesion.php';
                     
 
                     <div class="checkout__input text-center">
-                        <h3 class="h3">Información Extra<span>*</span></h3>
+                        <h3 class="h3">Información Extra<span></span></h3>
                     </div>
 
                     
@@ -240,14 +240,14 @@ include '../../inc/peticiones/login/sesion.php';
 
                     <div class="checkout__input__checkbox">
                         <label for="acc">
-                            <h5 class="h5">¿Cuentas con servicio a domicilio?<span>*</span></h5>
+                            <h5 class="h5">¿Cuentas con servicio a domicilio?<span></span></h5>
                             <input type="checkbox" id="acc">
                             <span class="checkmark"></span>
                         </label>
                     </div>
 
                     <div class="checkout__input text-center">
-                        <h3 class="h3">Categorias de tu restaurante<span>*</span></h3>
+                        <h3 class="h3">Categorias de tu restaurante<span></span></h3>
                     </div>
                     
                     <div class="row">
@@ -261,7 +261,7 @@ include '../../inc/peticiones/login/sesion.php';
                                                 <form id="">
                                                     <div class="row">
                                                         <div class="col-md-12 mx-auto">
-                                                            <h5 class="h5">Selecciona las categorias en las que entra tu restaurante!<span>*</span></h5>
+                                                            <h5 class="h5">Selecciona las categorias en las que entra tu restaurante!<span></span></h5>
                                                             <div class="input-group mb-3">
                                                                 <select placeholder="Ciudad" class="custom-select" name="cbx_categoria" id="cbx_categoria"></select>
                                                                 <div class="input-group-append">
@@ -321,105 +321,3 @@ include '../../inc/peticiones/login/sesion.php';
 
 </html>
 
-
-<!--<div class="row">
-
-                        <div class="col-lg-6">
-                            <div class="checkout__input">
-                                <p>Nombre del Restaurante<span>*</span></p>
-                                <input id="nombre" placeholder="Nombre" name="nombre" type="text" required="required">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="checkout__input">
-                                <p>Descripción Corta del Restaurante<span>*</span></p>
-                                <input id="desc_corta" name="desc_corta" type="text" required="required">
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="checkout__input">
-                        <p>Logotipo del restaurante<span>*</span></p>
-                        <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="imagen" lang="es" accept=".png,.jpg">
-                            <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label>
-                        </div>
-                        <br>
-                    </div>
-
-                    <div class="hero__search">
-                            <form id="eleccion_ciudad">
-                                <p>Selecciona tu ciudad:<span>*</span></p>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <select placeholder="Ciudad" class="custom-select" name="cbx" id="cbx"></select>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-
-
-                        <div class="checkout__input">
-                        
-                        <input placeholder="Dirección" id="direccion" name="direccion" type="text" class="checkout__input__add" required="required">
-
-                        <input placeholder="Codigo Postal" id="cp" name="cp" type="text" class="checkout__input__add" required="required">
-                    </div>
-
-
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="checkout__input">
-                                <p>Telefono<span>*</span></p>
-                                <input placeholder="Telefono" id="telefono" name="telefono" type="text" required="required">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="checkout__input">
-                                <p>Email<span>*</span></p>
-                                <input placeholder="Correo Electronico" id="email" name="email" type="text" required="required">
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="checkout__input">
-                                <p>Facebook<span>*</span></p>
-                                <input placeholder="URL o link de Facebook" id="facebook" name="facebook" type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="checkout__input">
-                                <p>Instagram<span>*</span></p>
-                                <input placeholder="URL o link de Instagram" id="instagram" name="instagram" type="text">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="checkout__input">
-                        <div class="row">
-                            <div class="col">
-                                <label> Horario Apertura</label>
-                                <input type="time" id="horario_abrir">
-                            </div>
-                            <div class="col">
-                                <label> Horario Cierre</label>
-                                <input type="time" id="horario_cerrar">
-                            </div>
-                        </div>
-                    </div>
-
-                    -->
-
-
-<!--
-                    <div class="checkout__input">
-                        <p>Ubicacion<span>*</span></p>
-                        <input type="text" placeholder="Estado" id="estado" name="estado" class="checkout__input__add" required="required">
-                        <input type="text" placeholder="Municipio" id="municipio" name="municipio" class="checkout__input__add" required="required">
-                        <input type="text" placeholder="Ciudad" id="ciudad" name="ciudad" class="checkout__input__add" required="required">
-                    </div>
-                    -->
