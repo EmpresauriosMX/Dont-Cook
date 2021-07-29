e8930055a04974861abf3146b17b82b301352159
samgongora26 <saul.santiago2426@gmail.com>
2021-07-29 02:26:09 -0500

    cambios de aviso de privacidad

--- templates/pages/aviso_privacidad.php
+++ templates/pages/aviso_privacidad.php
@@ -20,7 +20,7 @@
             <h1>Aviso de privacidad simplificado</h1>
         </div>
         <div class="card-body">
-            <h5 class="card-text">Daniel Gonzalez, Saul Gogora, Luis Tzun, Osly Rodriguez mejores conocidos como Don't Cook, con domicilio en Quintana Roo, México son los responsables del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</h5>
+            <h5 class="card-text">Daniel Gonzalez, Saul Góngora, Luis Tzun, Osly Rodriguez mejores conocidos como Don't Cook, con domicilio en Quintana Roo, México son los responsables del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</h5>
             <h3 class="card-title">¿Para qué fines utilizaremos sus datos personales?</h3>
             <h5 class="card-text">Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</h5>
             <h3 class="card-tittle">¿Dónde puedo consultar el aviso de privacidad integral?</h3>
@@ -38,16 +38,16 @@
             <h1>Aviso de privacidad integral</h1>
         </div>
         <div class="card-body">
-            <h5 class="card-text">Daniel Gonzalez, Saul Gogora, Luis Tzun, Osly Rodriguez mejores conocidos como Don't Cook, con domicilio en Solidaridad, Quintana Roo, México y portal de internet <a href="../home/home.php">dontcook.mx</a> , son los responsables del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</h5>
+            <h5 class="card-text">Daniel Gonzalez, Saul Góngora, Luis Tzun, Osly Rodriguez mejores conocidos como Don't Cook, con domicilio en Solidaridad, Quintana Roo, México y portal de internet <a href="../home/home.php">dontcook.mx</a> , son los responsables del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</h5>
             
             <h3 class="card-title text-center">¿Para qué fines utilizaremos sus datos personales?</h3>
             <h5 class="card-text">Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</h5>
             <h5 class="card-text">· Respuesta a mensajes del formulario de contacto</h5>
-            <h5 class="card-text">· Comerciales, Publicidad</h5>
+            <h5 class="card-text">· Comerciales, publicidad</h5>
 
             <h3 class="card-tittle text-center">¿Qué datos personales utilizaremos para estos fines?</h3>
             <h5 class="card-text">Para llevar a cabo las finalidades descritas en el presente aviso de privacidad, utilizaremos los siguientes datos personales:</h5>
-            <h5 class="cart-text">· Datos de identificación y contacto, Datos laborales</h5>
+            <h5 class="cart-text">· Datos de identificación, contacto y datos laborales</h5>
 
             <h3 class="card-tittle text-center">¿Con quién compartimos su información personal y para qué fines?</h3>
             <h5 class="card-text">Le informamos que sus datos personales son compartidos fuera del país con las siguientes personas, empresas, organizaciones o autoridades distintas a nosotros, para los siguientes fines:</h5>
@@ -64,7 +64,7 @@
             <h5 class="card-text">· Copia de una identificación oficial adjunta.</h5>
             <h5 class="card-text">· Asunto «Derechos ARCO»</h5>
 
-            <h5 class="card-text">Descripción el objeto del escrito, los cuales pueden ser de manera enunciativa más no limitativa los siguientes: Revocación del consentimiento para tratar sus datos personales; y/o Notificación del uso indebido del tratamiento de sus datos personales; y/o Ejercitar sus Derechos ARCO, con una descripción clara y precisa de los datos a Acceder, Rectificar, Cancelar o bien, Oponerse. En caso de Rectificación de datos personales, deberá indicar la modificación exacta y anexar la documentación soporte; es importante en caso de revocación del consentimiento, que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligación legal requiramos seguir tratando sus datos personales. Asimismo, usted deberá considerar que para ciertos fines, la revocación de su consentimiento implicará que no le podamos seguir prestando el servicio que nos solicitó, o la conclusión de su relación con nosotros.</h5>
+            <h5 class="card-text">Descripción el objeto del escrito, los cuales pueden ser de manera enunciativa más no limitativa los siguientes: Revocación del consentimiento para tratar sus datos personales; y/o notificación del uso indebido del tratamiento de sus datos personales; y/o ejercitar sus derechos ARCO, con una descripción clara y precisa de los datos a aceder, rectificar, cancelar o bien,oponerse. En caso de rectificación de datos personales, deberá indicar la modificación exacta y anexar la documentación soporte; es importante en caso de revocación del consentimiento, que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligación legal requiramos seguir tratando sus datos personales. Asimismo, usted deberá considerar que para ciertos fines, la revocación de su consentimiento implicará que no le podamos seguir prestando el servicio que nos solicitó, o la conclusión de su relación con nosotros.</h5>
             <h5 class="card-text text-center">¿En cuántos días le daremos respuesta a su solicitud?</h5>
             <h5 class="card-text">5 Días</h5>
             <h5 class="card-text text-center">¿Por qué medio le comunicaremos la respuesta a su solicitud?</h5>
@@ -72,7 +72,7 @@
 
             <h3 class="card-tittle text-center">El uso de tecnologías de rastreo en nuestro portal de internet</h3>
             <h5 class="card-text">Le informamos que en nuestra página de internet utilizamos cookies, web beacons u otras tecnologías, a través de las cuales es posible monitorear su comportamiento como usuario de internet, así como brindarle un mejor servicio y experiencia al navegar en nuestra página. Los datos personales que obtenemos de estas tecnologías de rastreo son los siguientes:</h5>
-            <h5 class="card-text">Identificadores, nombre de usuario y contraseñas de sesión, Región en la que se encuentra el usuario, Fecha y hora del inicio y final de una sesión de un usuario</h5>
+            <h5 class="card-text">Identificadores, nombre de usuario y contraseñas de sesión, región en la que se encuentra el usuario, fecha y hora del inicio y final de una sesión de un usuario.</h5>
             <h5 class="card-text">Estas cookies, web beacons y otras tecnologías pueden ser deshabilitadas. Para conocer cómo hacerlo, consulte el menú de ayuda de su navegador. Tenga en cuenta que, en caso de desactivar las cookies, es posible que no pueda acceder a ciertas funciones personalizadas en nuestros sitio web.</h5>
 
             <h3 class="card-tittle text-center">¿Cómo puede conocer los cambios en este aviso de privacidad?</h3>
