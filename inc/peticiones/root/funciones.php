56e29996cdf60a07389e00111f50533f25139c2b
samgongora26 <saul.santiago2426@gmail.com>
2021-07-31 20:13:47 -0500

    ptm aver

--- inc/peticiones/root/funciones.php
+++ inc/peticiones/root/funciones.php
@@ -12,6 +12,63 @@ switch ($accion) {
     case "cambiar_estado_usuario":
         $resultado = cambiar_estado_usuario();
         break;
+    case "info_restaurantes":
+        $resultado = info_restaurante();
+        break;
+     case "agregar_promo":
+        $resultado = agregar_promocion();
+        break;
+    case "ver_promo":
+        $resultado = ver_promocion();
+        break;
+    case "ver_promo_especifico":
+        $resultado = ver_promo_especifico();
+        break;
+    case "editar_promo":
+        $resultado = editar_promo();
+        break;
+    case "eliminar_promocion":
+        $resultado = eliminar_promocion();
+        break;
+    case "obtener_categorias":
+        $resultado = obtener_categorias();
+        break;
+    case "horario_restaurante":
+        $resultado = horario_restaurante();
+        break;
+    case "actualiza_datos_generales":
+        $resultado = actualiza_datos_generales();
+        break;
+    case "actualiza_datos_contacto":
+        $resultado = actualiza_datos_contacto();
+        break;
+    case "actualiza_datos_ciudad":
+        $resultado = actualiza_datos_ciudad();
+        break;
+    case "actualiza_datos_horario":
+        $resultado = actualiza_datos_horario();
+        break;
+    case "actualiza_datos_categoria":
+        $resultado = actualiza_datos_categoria();
+        break;
+    case 'subir_menu':
+        $resultado = subir_menu();
+        break;
+    case 'actualizar_menu':
+        $resultado = actualizar_menu();
+        break;
+    case 'mostrar_menu':
+        $resultado = mostrar_menu();
+        break;
+    case 'cambiar_imagen_menu':
+        $resultado = cambiar_imagen_menu();
+        break;
+        case 'cambiar_imagen_restaurante':
+        $resultado = cambiar_imagen_restaurante();
+        break;
+    case 'cambiar_imagen_promocion':
+        $resultado = cambiar_imagen_promocion();
+        break;
 }
 
 
