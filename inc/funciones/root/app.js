21a07a9b14cf130c9870bae67e84800be767c428
feltydany <feltydany.dgm@gmail.com>
2021-07-31 22:29:21 -0500

    avance
    Signed-off-by: feltydany <feltydany.dgm@gmail.com>

--- inc/funciones/root/app.js
+++ inc/funciones/root/app.js
@@ -4,11 +4,27 @@ const url = "../../inc/peticiones/root/funciones.php";
 const contenido_agregar = document.querySelector("#form_agregar");
 const contenido_mostrar = document.querySelector("#form_categoria");
 
+const contenedor_eliminar = document.querySelector("#contenedor_eliminar");
+const contenedor_editar = document.querySelector("#contenedor_editar");
+
+let imagen_a_enviar = document.querySelector("#imagen"); //input
+let imagen_previa = document.querySelector("#img_previa");
 
 document.addEventListener("DOMContentLoaded", () => {
     contenido_agregar.addEventListener("submit",guardar);
     mostrar_categoria();
+    contenedor_eliminar.addEventListener("click",eliminar);
+    contenedor_editar.addEventListener("click",editar);
 });
+
+
+async function eliminar(){
+    
+}
+
+async function editar(){
+    
+}
 
 async function guardar(){
 
@@ -57,8 +73,8 @@ async function imprime_categorias(res) {
             <img class="card-img-top" src="../../src/img/banner/banner_dc5.png" alt="Card image cap">
             <div class="card-body"></div>
                 <h5 name="nombre" id="nombre" class="card-title">${nombre}</h5>
-                <button class="btn btn-dark btn-lg btn-block">Editar</button> 
-                <button class="btn btn-danger btn-lg btn-block">Eliminar</button>    
+                    <button id="contenedor_editar" class="btn btn-dark btn-lg btn-block">Editar</button> 
+                    <button id="contenedor_eliminar" class="btn btn-danger btn-lg btn-block">Eliminar</button>
             </div>
         </div>
         `;
